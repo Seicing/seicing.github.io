1598d6761af4014afa01294945ff70e22c707e95
Seicing <1289846385@qq.com>
2022-01-01 02:02:53 +0800

    2022-01-01-02:02:51

--- js/essay.js
+++ js/essay.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
 })
 
 function overstep(a, b) {
-    document.getElementById("y2019").style.display = "block";
-    document.getElementById("y2020").style.display = "block";
-    document.getElementById("y2021").style.display = "block";
-    document.getElementById("y2022").style.display = "block";
+    document.getElementById("hajime").style.display = "block";
+    document.getElementById("hatten").style.display = "block";
+    document.getElementById("tsuzuku").style.display = "block";
+    document.getElementById("hanei").style.display = "block";
     document.getElementById(a).style.display = "none";
-    document.getElementById("y2019div").style.display = "none";
-    document.getElementById("y2020div").style.display = "none";
-    document.getElementById("y2021div").style.display = "none";
-    document.getElementById("y2022div").style.display = "none";
+    document.getElementById("hajimediv").style.display = "none";
+    document.getElementById("hattendiv").style.display = "none";
+    document.getElementById("tsuzukudiv").style.display = "none";
+    document.getElementById("haneidiv").style.display = "none";
     document.getElementById(b).style.display = "block";
 }
