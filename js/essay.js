2e12d2281c432963620a2b3343d334a4bc096c79
Seicing <1289846385@qq.com>
2022-01-01 02:12:49 +0800

    2022-01-01-02:12:47

--- js/essay.js
+++ js/essay.js
@@ -3,10 +3,10 @@ $(document).ready(function () {
 })
 
 function overstep(a, b) {
-    document.getElementById("hajime").style.display = "block";
-    document.getElementById("hatten").style.display = "block";
-    document.getElementById("tsuzuku").style.display = "block";
-    document.getElementById("hanei").style.display = "block";
+    document.getElementById("hajimebutton").style.display = "block";
+    document.getElementById("hattenbutton").style.display = "block";
+    document.getElementById("tsuzukubutton").style.display = "block";
+    document.getElementById("haneibutton").style.display = "block";
     document.getElementById(a).style.display = "none";
     document.getElementById("hajimediv").style.display = "none";
     document.getElementById("hattendiv").style.display = "none";
