9d132744a18ceb12a30c687effedbf6688e67733
Seicing <1289846385@qq.com>
2022-01-01 02:26:51 +0800

    2022-01-01-02:26:49

--- js/essay.js
+++ js/essay.js
@@ -1,3 +1,21 @@
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) { return pair[1]; }
+    }
+    return (false);
+}
+
+window.onload = tipsp()
+
+function tipsp() {
+    var tipsp1 = getQueryVariable("nenbun")
+    var tipsp2 = tipsp1 + "button";
+    document.getElementById(tipsp2).click();
+}
+
 $(document).ready(function () {
     $('#sidebar').load('https://seicing.com/js/list/essay.html');
 })
