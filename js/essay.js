5a8366eae9871f5a67c7cb00486cdf6eb6681c78
Seicing <1289846385@qq.com>
2022-01-01 01:25:38 +0800

    2022-01-01-01:25:36

--- js/essay.js
+++ js/essay.js
@@ -2,13 +2,6 @@ $(document).ready(function () {
     $('#sidebar').load('https://seicing.com/js/list/essay.html');
 })
 
-window.onload = tipsp()
-function tipsp() {
-    var tipsp1 = getQueryVariable("year")
-    document.getElementById(tipsp1).click();
-}
-
-
 function overstep(a, b) {
     document.getElementById("y2019").style.display = "block";
     document.getElementById("y2020").style.display = "block";
