c3527c1bc9b947cccc77e0a4dfe536b3e448d00c
Seicing <1289846385@qq.com>
2022-01-01 16:30:49 +0800

    2022-01-01-16:30:48

--- js/essay.js
+++ js/essay.js
@@ -16,20 +16,31 @@ $(document).ready(function () {
     var tipsp1 = getQueryVariable("nenbun");
     var tipsp2 = tipsp1 + "button";
     var tipsp3 = tipsp1 + "div";
-
-    function overstep(tipsp2, tipsp3) {
-        document.getElementById("hajimebutton").style.display = "block";
-        document.getElementById("hattenbutton").style.display = "block";
-        document.getElementById("tsuzukubutton").style.display = "block";
-        document.getElementById("haneibutton").style.display = "block";
-        document.getElementById(a).style.display = "none";
-        document.getElementById("hajimediv").style.display = "none";
-        document.getElementById("hattendiv").style.display = "none";
-        document.getElementById("tsuzukudiv").style.display = "none";
-        document.getElementById("haneidiv").style.display = "none";
-        document.getElementById(b).style.display = "block";
-    }
+    document.getElementById("hajimebutton").style.display = "block";
+    document.getElementById("hattenbutton").style.display = "block";
+    document.getElementById("tsuzukubutton").style.display = "block";
+    document.getElementById("haneibutton").style.display = "block";
+    document.getElementById(tipsp2).style.display = "none";
+    document.getElementById("hajimediv").style.display = "none";
+    document.getElementById("hattendiv").style.display = "none";
+    document.getElementById("tsuzukudiv").style.display = "none";
+    document.getElementById("haneidiv").style.display = "none";
+    document.getElementById(tipsp3).style.display = "block";
 })
+
+
+function overstep(a, b) {
+    document.getElementById("hajimebutton").style.display = "block";
+    document.getElementById("hattenbutton").style.display = "block";
+    document.getElementById("tsuzukubutton").style.display = "block";
+    document.getElementById("haneibutton").style.display = "block";
+    document.getElementById(a).style.display = "none";
+    document.getElementById("hajimediv").style.display = "none";
+    document.getElementById("hattendiv").style.display = "none";
+    document.getElementById("tsuzukudiv").style.display = "none";
+    document.getElementById("haneidiv").style.display = "none";
+    document.getElementById(b).style.display = "block";
+}
 
 //window.onload = tipsp()
 
