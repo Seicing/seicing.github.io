80b6e5b0011f90639c81732274b6c7be2d1ad698
Seicing <1289846385@qq.com>
2022-01-01 16:22:03 +0800

    2022-01-01-16:22:01

--- js/essay.js
+++ js/essay.js
@@ -1,19 +1,34 @@
-function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split("=");
-        if (pair[0] == variable) { return pair[1]; }
-    }
-    return (false);
-}
+
 
 $(document).ready(function () {
     $('#sidebar').load('https://seicing.com/js/list/essay.html');
-    var tipsp1 = getQueryVariable("nenbun")
+
+    function getQueryVariable(variable) {
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split("=");
+            if (pair[0] == variable) { return pair[1]; }
+        }
+        return (false);
+    }
+
+    var tipsp1 = getQueryVariable("nenbun");
     var tipsp2 = tipsp1 + "button";
     var tipsp3 = tipsp1 + "div";
-    overstep(tipsp2, tipsp3)
+
+    function overstep(tipsp2, tipsp3) {
+        document.getElementById("hajimebutton").style.display = "block";
+        document.getElementById("hattenbutton").style.display = "block";
+        document.getElementById("tsuzukubutton").style.display = "block";
+        document.getElementById("haneibutton").style.display = "block";
+        document.getElementById(a).style.display = "none";
+        document.getElementById("hajimediv").style.display = "none";
+        document.getElementById("hattendiv").style.display = "none";
+        document.getElementById("tsuzukudiv").style.display = "none";
+        document.getElementById("haneidiv").style.display = "none";
+        document.getElementById(b).style.display = "block";
+    }
 })
 
 //window.onload = tipsp()
@@ -24,15 +39,3 @@ $(document).ready(function () {
 //    document.getElementById(tipsp2).click();
 //}
 
-function overstep(a, b) {
-    document.getElementById("hajimebutton").style.display = "block";
-    document.getElementById("hattenbutton").style.display = "block";
-    document.getElementById("tsuzukubutton").style.display = "block";
-    document.getElementById("haneibutton").style.display = "block";
-    document.getElementById(a).style.display = "none";
-    document.getElementById("hajimediv").style.display = "none";
-    document.getElementById("hattendiv").style.display = "none";
-    document.getElementById("tsuzukudiv").style.display = "none";
-    document.getElementById("haneidiv").style.display = "none";
-    document.getElementById(b).style.display = "block";
-}
