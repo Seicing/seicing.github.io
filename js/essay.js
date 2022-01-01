f271255774fc5d736c064f9f9158ca0da0a9a712
Seicing <1289846385@qq.com>
2022-01-01 19:46:12 +0800

    2022-01-01-19:46:10

--- js/essay.js
+++ js/essay.js
@@ -11,7 +11,8 @@ function getQueryVariable(variable) {
 
 
 $(document).ready(function () {
-    $('#sidebar').load('https://seicing.com/js/list/essay.html').load = function () {
+    $('#sidebar').load('https://seicing.com/js/list/essay.html'), function () {
+        alert("扑街啊");
         var tipsp1 = getQueryVariable("nenbun")
         console.log("tipsp1 OK")
         var tipsp2 = tipsp1 + "button"
