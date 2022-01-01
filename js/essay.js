943ced338e5bc9eb3e27c52521f8eb70ddbee020
Seicing <1289846385@qq.com>
2022-01-01 18:51:36 +0800

    2022-01-01-18:51:35

--- js/essay.js
+++ js/essay.js
@@ -1,28 +1,6 @@
-
-
-function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split("=");
-        if (pair[0] == variable) { return pair[1]; }
-    }
-    return (false);
-}
-
 $(document).ready(function () {
     $('#sidebar').load('https://seicing.com/js/list/essay.html');
 })
-
-function cnmsb() {
-    document.getElementById("essaybu").innerHTML = "<img src='https://seicing.com/res/dex.jpg' id='dex'><a href='https://seicing.com/html/index.html'><b>回到首页</b></a>";
-    document.getElementById("dex").onload = function () {
-        var tipsp1 = getQueryVariable("nenbun")
-        var tipsp2 = tipsp1 + "button";
-        document.getElementById(tipsp2).click();
-    }
-}
-
 
 function overstep(a, b) {
     document.getElementById("hajimebutton").style.display = "block";
