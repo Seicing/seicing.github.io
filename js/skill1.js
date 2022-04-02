25e489e75cb644221f182391558589803eba21b8
Seicing <1289846385@qq.com>
2022-04-02 14:35:17 +0800

    2022-04-02-14:35:12

--- js/skill1.js
+++ js/skill1.js
@@ -5,7 +5,7 @@ function showPic(e, taitou, kontie) {
     aasb = document.getElementById("Layer1")
     aasb.style.display = "";
     aasb.style.width = "200px";
-    aasb.innerHTML = "<div style='background:url(https://seicing.coding.net/p/elitefatcat/d/elitefatcat/git/raw/master/header.png) no-repeat;padding:7px 7px 0px 7px;'><font style='color:#ffc400;font-family: SimSun;'>" + taitou + "</font><br><img src='https://seicing.coding.net/p/elitefatcat/d/elitefatcat/git/raw/master/line.png'></div><div style='background:url(https://seicing.coding.net/p/elitefatcat/d/elitefatcat/git/raw/master/midder.png) repeat-y;padding:0px 7px 0px 7px;'><font style='color:#ffffff;font-family: SimSun;'>" + kontie + "</font></div>  <div style='background:url(https://seicing.coding.net/p/elitefatcat/d/elitefatcat/git/raw/master/ender.png) no-repeat;'><font style='color:#ffffff;'><p>&nbsp</p></font></div>";
+    aasb.innerHTML = "<div style='background:url(https://seicing-1257171891.cos.ap-nanjing.myqcloud.com/2fatcatpool/header.png) no-repeat;padding:7px 7px 0px 7px;'><font style='color:#ffc400;font-family: SimSun;'>" + taitou + "</font><br><img src='https://seicing-1257171891.cos.ap-nanjing.myqcloud.com/2fatcatpool/line.png'></div><div style='background:url(https://seicing-1257171891.cos.ap-nanjing.myqcloud.com/2fatcatpool/midder.png) repeat-y;padding:0px 7px 0px 7px;'><font style='color:#ffffff;font-family: SimSun;'>" + kontie + "</font></div>  <div style='background:url(https://seicing-1257171891.cos.ap-nanjing.myqcloud.com/2fatcatpool/ender.png) no-repeat;'><font style='color:#ffffff;'><p>&nbsp</p></font></div>";
     var div = aasb;
     var z = div.offsetWidth;
     if (x + z < document.body.clientWidth) {
