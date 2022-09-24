ce1473ba2c70ac9416a0cc91c23bcba4bfbdaba1
Seicing <1289846385@qq.com>
2022-09-24 23:07:21 +0800

    2022-09-24-23:07:19

--- js/tech.js
+++ js/tech.js
@@ -1,13 +1,23 @@
 function showPic(e, taitou) {
+
+    $.ajax({
+        type: "get",
+        url: "https://seicing.com/js/dia/aoe4.json",
+    })
+
+    var ayanami = obj[taitou]
+
     var x, y, aasb;
     x = e.pageX;
     y = e.pageY;
-    aasb = document.getElementById("Layer1")
+
+    aasb = document.getElementById("Layer1");
     aasb.style.display = "";
     aasb.style.width = "200px";
-    aasb.innerHTML = "<div style='background:rgba(0,0,0,0.75)'><font style='color:#ffffff'>" + taitou + "</font></div>";
+    aasb.innerHTML = "<div style='background:rgba(0,0,0,0.75)'><font style='color:#ffffff'>" + ayanami + "</font></div>";
     var div = aasb;
     var z = div.offsetWidth;
+
     if (x + z < document.body.clientWidth) {
         aasb.style.left = x + 2 + 'px';
         aasb.style.top = y + 2 + 'px';
@@ -15,6 +25,7 @@ function showPic(e, taitou) {
         aasb.style.left = x - (x + z - document.body.clientWidth) + 'px';
         aasb.style.top = y + 2 + 'px';
     }
+
 }
 
 function hiddenPic() {
