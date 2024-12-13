4a67fed031194880d795cfa9adc231b52e21bf8e
Seicing <1289846385@qq.com>
2024-12-13 23:06:22 +0800

    2024-12-13-23:06:21

--- js/lavipage2.js
+++ js/lavipage2.js
@@ -1,5 +1,5 @@
 var lavipage = document.getElementById("lavipage");
 var page = lavipage.getAttribute("page");
 var label = document.getElementById("page_" + page);
-label.style.color = "#6b1e1e";
+label.style.color = "#1e1e64";
 label.style.fontWeight = "bold";
