687b01371805018c7b7105d13b611f538dacca99
Seicing <1289846385@qq.com>
2025-10-10 01:57:13 +0800

    2025-10-10-01:57:11

--- js/aoe4civ/aoe4list_hre.js
+++ js/aoe4civ/aoe4list_hre.js
@@ -1,401 +1,394 @@
 export const aoe4list = [
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "平民与宗教单位",
     "name": "村民",
     "img": "村民",
     "link": "https://seicing.com/html/aoe2/unitaoe4/村民"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "平民与宗教单位",
-    "name": "僧侣",
-    "img": "僧侣",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/僧侣"
+    "name": "高级教士",
+    "img": "高级教士",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/高级教士"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "平民与宗教单位",
     "name": "商人",
     "img": "商人",
     "link": "https://seicing.com/html/aoe2/unitaoe4/商人"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "平民与宗教单位",
     "name": "君主",
     "img": "君主",
     "link": "https://seicing.com/html/aoe2/unitaoe4/君主"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "军事单位",
     "name": "长矛兵",
     "img": "长矛兵",
     "link": "https://seicing.com/html/aoe2/unitaoe4/长矛兵"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "军事单位",
     "name": "武士",
     "img": "武士",
     "link": "https://seicing.com/html/aoe2/unitaoe4/武士"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
+    "section": "军事单位",
+    "name": "国土佣仆",
+    "img": "国土佣仆",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/国土佣仆"
+  },
+  {
+    "civ": "hre",
     "section": "军事单位",
     "name": "侦察兵",
     "img": "侦察兵",
     "link": "https://seicing.com/html/aoe2/unitaoe4/侦察兵"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "军事单位",
     "name": "骑手",
     "img": "骑手",
     "link": "https://seicing.com/html/aoe2/unitaoe4/骑手"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "军事单位",
-    "name": "皇家骑士",
-    "img": "皇家骑士",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/皇家骑士"
+    "name": "骑士",
+    "img": "骑士",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/骑士"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "军事单位",
     "name": "步弓手",
     "img": "步弓手",
     "link": "https://seicing.com/html/aoe2/unitaoe4/步弓手"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "军事单位",
-    "name": "弓弩手",
-    "img": "弓弩手",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/弓弩手"
+    "name": "弩手",
+    "img": "弩手",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/弩手"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "军事单位",
     "name": "火枪兵",
     "img": "火枪兵",
     "link": "https://seicing.com/html/aoe2/unitaoe4/火枪兵"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
+    "section": "军事单位",
+    "name": "黑骑兵",
+    "img": "黑骑兵",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/黑骑兵"
+  },
+  {
+    "civ": "hre",
     "section": "攻城武器",
     "name": "扭力弩炮",
     "img": "扭力弩炮",
     "link": "https://seicing.com/html/aoe2/unitaoe4/扭力弩炮"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "攻城武器",
     "name": "轻型投石车",
     "img": "轻型投石车",
     "link": "https://seicing.com/html/aoe2/unitaoe4/轻型投石车"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "攻城武器",
     "name": "配重式巨型投石机",
     "img": "配重式巨型投石机",
     "link": "https://seicing.com/html/aoe2/unitaoe4/配重式巨型投石机"
   },
   {
-    "civ": "fre",
-    "section": "攻城武器",
-    "name": "加农炮",
-    "img": "加农炮",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/加农炮"
-  },
-  {
-    "civ": "fre",
+    "civ": "hre",
     "section": "攻城武器",
     "name": "长管炮",
     "img": "长管炮",
     "link": "https://seicing.com/html/aoe2/unitaoe4/长管炮"
   },
   {
-    "civ": "fre",
-    "section": "攻城武器",
-    "name": "风琴炮",
-    "img": "风琴炮",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/风琴炮"
-  },
-  {
-    "civ": "fre",
+    "civ": "hre",
     "section": "攻城武器",
     "name": "攻城塔",
     "img": "攻城塔",
     "link": "https://seicing.com/html/aoe2/unitaoe4/攻城塔"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "攻城武器",
     "name": "攻城锤",
     "img": "攻城锤",
     "link": "https://seicing.com/html/aoe2/unitaoe4/攻城锤"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "船只",
     "name": "渔船",
     "img": "渔船",
     "link": "https://seicing.com/html/aoe2/unitaoe4/渔船"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "船只",
     "name": "商船",
     "img": "商船",
     "link": "https://seicing.com/html/aoe2/unitaoe4/商船"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "船只",
     "name": "运输船",
     "img": "运输船",
     "link": "https://seicing.com/html/aoe2/unitaoe4/运输船"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "船只",
     "name": "箭船",
     "img": "箭船",
     "link": "https://seicing.com/html/aoe2/unitaoe4/箭船"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "船只",
-    "name": "柯克战船",
-    "img": "柯克战船",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/柯克战船"
+    "name": "趸船",
+    "img": "趸船",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/趸船"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "船只",
     "name": "爆破船",
     "img": "爆破船",
     "link": "https://seicing.com/html/aoe2/unitaoe4/爆破船"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "船只",
     "name": "大帆船",
     "img": "大帆船",
     "link": "https://seicing.com/html/aoe2/unitaoe4/大帆船"
   },
   {
-    "civ": "fre",
-    "section": "船只",
-    "name": "三桅战船",
-    "img": "三桅战船",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/三桅战船"
-  },
-  {
-    "civ": "fre",
+    "civ": "hre",
     "section": "黑暗时代建筑",
     "name": "房屋",
     "img": "房屋",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/房屋"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "黑暗时代建筑",
     "name": "磨坊",
     "img": "磨坊",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/磨坊"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "黑暗时代建筑",
     "name": "伐木场",
     "img": "伐木场",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/伐木场"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "黑暗时代建筑",
     "name": "采矿场",
     "img": "采矿场",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/采矿场"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "黑暗时代建筑",
     "name": "农田",
     "img": "农田",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/农田"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "黑暗时代建筑",
     "name": "兵营",
     "img": "兵营",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/兵营"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "黑暗时代建筑",
     "name": "码头",
     "img": "码头",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/码头"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "黑暗时代建筑",
     "name": "哨站",
     "img": "哨站",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/哨站"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "黑暗时代建筑",
     "name": "木栅栏",
     "img": "木栅栏",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/木栅栏"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "黑暗时代建筑",
     "name": "木城门",
     "img": "木城门",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/木城门"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "封建时代建筑",
     "name": "铁匠铺",
     "img": "铁匠铺",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/铁匠铺"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "封建时代建筑",
     "name": "市场",
     "img": "市场",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/市场"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "封建时代建筑",
     "name": "城镇中心",
     "img": "城镇中心",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/城镇中心"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "封建时代建筑",
     "name": "靶场",
     "img": "靶场",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/靶场"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "封建时代建筑",
     "name": "马厩",
     "img": "马厩",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/马厩"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "封建时代建筑",
     "name": "石墙塔",
     "img": "石墙塔",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/石墙塔"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "封建时代建筑",
     "name": "石墙",
     "img": "石墙",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/石墙"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "封建时代建筑",
     "name": "石墙城门",
     "img": "石墙城门",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/石墙城门"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "城堡时代建筑",
     "name": "修道院",
     "img": "修道院",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/修道院"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "城堡时代建筑",
     "name": "攻城武器厂",
     "img": "攻城武器厂",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/攻城武器厂"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "城堡时代建筑",
     "name": "城堡",
     "img": "城堡",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/城堡"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "帝王时代建筑",
     "name": "大学",
     "img": "大学",
     "link": "https://seicing.com/html/aoe2/buildingsaoe4/大学"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "帝王时代建筑",
-    "name": "圣母院",
-    "img": "圣母院",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/圣母院"
+    "name": "弗伦斯堡大宫殿",
+    "img": "弗伦斯堡大宫殿",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/弗伦斯堡大宫殿"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "地标建筑",
-    "name": "商会",
-    "img": "商会",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/商会"
+    "name": "迈因维克宫殿",
+    "img": "迈因维克宫殿",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/迈因维克宫殿"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "地标建筑",
-    "name": "骑兵学校",
-    "img": "骑兵学校",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/骑兵学校"
+    "name": "亚琛教堂",
+    "img": "亚琛教堂",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/亚琛教堂"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "地标建筑",
-    "name": "皇家学院",
-    "img": "皇家学院",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/皇家学院"
+    "name": "雷格尼茨大教堂",
+    "img": "雷格尼茨大教堂",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/雷格尼茨大教堂"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "地标建筑",
-    "name": "公会大厅",
-    "img": "公会大厅",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/公会大厅"
+    "name": "城主宫殿",
+    "img": "城主宫殿",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/城主宫殿"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "地标建筑",
-    "name": "炮兵学院",
-    "img": "炮兵学院",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/炮兵学院"
+    "name": "施瓦本宫殿",
+    "img": "施瓦本宫殿",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/施瓦本宫殿"
   },
   {
-    "civ": "fre",
+    "civ": "hre",
     "section": "地标建筑",
-    "name": "红宫",
-    "img": "红宫",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/红宫"
+    "name": "埃尔茨巴赫宫殿",
+    "img": "埃尔茨巴赫宫殿",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/埃尔茨巴赫宫殿"
   },
 ];
