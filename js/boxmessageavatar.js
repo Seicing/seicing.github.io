51d74baa5910fde3ac1f65a37296da0b109ac2eb
Seicing <1289846385@qq.com>
2026-02-08 13:56:13 +0800

    2026-02-08-13:56:12

--- js/boxmessageavatar.js
+++ js/boxmessageavatar.js
@@ -1,21 +1,54 @@
-var listText = ['atext', 'btext', 'ctext', 'dtext', 'etext', 'ftext', 'gtext', 'htext', 'itext', 'jtext', 'ktext', 'ltext', 'mtext', 'ntext', 'otext', 'ptext', 'qtext', 'rtext', 'stext', 'ttext', 'utext']
-var listButton = ['abutton', 'bbutton', 'cbutton', 'dbutton', 'ebutton', 'fbutton', 'gbutton', 'hbutton', 'ibutton', 'jbutton', 'kbutton', 'lbutton', 'mbutton', 'nbutton', 'obutton', 'pbutton', 'qbutton', 'rbutton', 'sbutton', 'tbutton', 'ubutton']
+// 1. 定义 ID 前缀列表 (对应你 HTML 中的 a, b, c ... u)
+var listPrefix = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u'];
 
-function tipsg(a, b, c) {
-    cleargasek(c);
-    document.getElementById(a).style.display = "block";
-    document.getElementById(b).style.color = "blue";
-    $("#" + a + " img").each(function () {
-        var src = $(this).attr("data-src");
-        $(this).attr("src", src);
-    });
-}
+function tipsg(targetTextId, targetButtonId, suffix) {
+    // --- 第一步：重置状态 (隐藏所有内容，灰显所有按钮) ---
+    // 遍历所有前缀，拼接后缀来找到页面元素
+    for (var i = 0; i < listPrefix.length; i++) {
+        var prefix = listPrefix[i];
+        var tempTextId = prefix + 'text' + suffix;   // 例如 atext0
+        var tempBtnId = prefix + 'button' + suffix; // 例如 abutton0
 
-function cleargasek(a) {
-    for (var i = 0; i < listText.length; i++) {
-        document.getElementById(listText[i] + a).style.display = "none";
+        var elText = document.getElementById(tempTextId);
+        var elBtn = document.getElementById(tempBtnId);
+
+        // 隐藏内容
+        if (elText) {
+            elText.style.display = "none";
+        }
+        // 恢复按钮颜色为灰色
+        if (elBtn) {
+            elBtn.style.color = "#d0c9b7";
+        }
     }
-    for (var g = 0; g < listButton.length; g++) {
-        document.getElementById(listButton[g] + a).style.color = "#d0c9b7";
+
+    // --- 第二步：激活当前选中的部分 ---
+    var currentText = document.getElementById(targetTextId);
+    var currentBtn = document.getElementById(targetButtonId);
+
+    if (currentText) {
+        currentText.style.display = "block";
+    }
+    if (currentBtn) {
+        currentBtn.style.color = "blue"; // 高亮当前按钮
+    }
+
+    // --- 第三步：核心懒加载逻辑 (只针对当前显示的 Div) ---
+    // 使用 jQuery 查找当前 targetTextId 下的所有图片
+    if (currentText) {
+        $("#" + targetTextId + " img").each(function () {
+            var $this = $(this);
+            var realSrc = $this.attr("data-src");
+
+            // 关键判断：
+            // 1. 图片必须有 data-src 属性
+            // 2. 图片当前的 src 不等于 data-src (说明还没加载过)
+            if (realSrc && $this.attr("src") !== realSrc) {
+                $this.attr("src", realSrc); // 这一步执行后，浏览器才开始下载图片
+
+                // (可选) 增加一点淡入效果，让加载看起来更平滑
+                $this.hide().fadeIn(300);
+            }
+        });
     }
 }
