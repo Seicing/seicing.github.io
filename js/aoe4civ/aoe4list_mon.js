ece6250ea0fc8415ce52a0b78cc752a6ab4fbc32
Seicing <1289846385@qq.com>
2026-03-08 09:33:01 +0800

    2026-03-08-09:32:59

--- js/aoe4civ/aoe4list_mon.js
+++ js/aoe4civ/aoe4list_mon.js
@@ -1,345 +1,345 @@
 export const aoe4list = [
-  {
-    "civ": "mon",
-    "section": "平民与宗教单位",
-    "name": "村民",
-    "img": "蒙古村民",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/村民"
-  },
-  {
-    "civ": "mon",
-    "section": "平民与宗教单位",
-    "name": "萨满",
-    "img": "萨满",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/萨满"
-  },
-  {
-    "civ": "mon",
-    "section": "平民与宗教单位",
-    "name": "商人",
-    "img": "商人",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/商人"
-  },
-  {
-    "civ": "mon",
-    "section": "平民与宗教单位",
-    "name": "君主",
-    "img": "君主",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/君主"
-  },
-  {
-    "civ": "mon",
-    "section": "军事单位",
-    "name": "长矛兵",
-    "img": "长矛兵",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/长矛兵"
-  },
-  {
-    "civ": "mon",
-    "section": "军事单位",
-    "name": "武士",
-    "img": "武士",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/武士"
-  },
-  {
-    "civ": "mon",
-    "section": "军事单位",
-    "name": "侦察兵",
-    "img": "侦察兵",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/侦察兵"
-  },
-  {
-    "civ": "mon",
-    "section": "军事单位",
-    "name": "骑手",
-    "img": "骑手",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/骑手"
-  },
-  {
-    "civ": "mon",
-    "section": "军事单位",
-    "name": "怯薛骑兵",
-    "img": "怯薛骑兵",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/怯薛骑兵"
-  },
-  {
-    "civ": "mon",
-    "section": "军事单位",
-    "name": "步弓手",
-    "img": "步弓手",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/步弓手"
-  },
-  {
-    "civ": "mon",
-    "section": "军事单位",
-    "name": "弩手",
-    "img": "弩手",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/弩手"
-  },
-  {
-    "civ": "mon",
-    "section": "军事单位",
-    "name": "火枪兵",
-    "img": "火枪兵",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/火枪兵"
-  },
-  {
-    "civ": "mon",
-    "section": "军事单位",
-    "name": "蒙古突骑",
-    "img": "蒙古突骑",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/蒙古突骑"
-  },
-  {
-    "civ": "mon",
-    "section": "军事单位",
-    "name": "汗的猎人",
-    "img": "汗的猎人",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/汗的猎人"
-  },
-  {
-    "civ": "mon",
-    "section": "军事单位",
-    "name": "汗",
-    "img": "汗",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/汗"
-  },
-  {
-    "civ": "mon",
-    "section": "攻城武器",
-    "name": "扭力弩炮",
-    "img": "扭力弩炮",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/扭力弩炮"
-  },
-  {
-    "civ": "mon",
-    "section": "攻城武器",
-    "name": "轻型投石车",
-    "img": "轻型投石车",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/轻型投石车"
-  },
-  {
-    "civ": "mon",
-    "section": "攻城武器",
-    "name": "牵引式巨型投石机",
-    "img": "牵引式巨型投石机",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/牵引式巨型投石机"
-  },
-  {
-    "civ": "mon",
-    "section": "攻城武器",
-    "name": "手推炮",
-    "img": "手推炮",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/手推炮"
-  },
-  {
-    "civ": "mon",
-    "section": "攻城武器",
-    "name": "攻城塔",
-    "img": "攻城塔",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/攻城塔"
-  },
-  {
-    "civ": "mon",
-    "section": "攻城武器",
-    "name": "攻城锤",
-    "img": "攻城锤",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/攻城锤"
-  },
-  {
-    "civ": "mon",
-    "section": "攻城武器",
-    "name": "回回炮",
-    "img": "回回炮",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/回回炮"
-  },
-  {
-    "civ": "mon",
-    "section": "船只",
-    "name": "渔船",
-    "img": "渔船",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/渔船"
-  },
-  {
-    "civ": "mon",
-    "section": "船只",
-    "name": "商船",
-    "img": "商船",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/商船"
-  },
-  {
-    "civ": "mon",
-    "section": "船只",
-    "name": "运输船",
-    "img": "运输船",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/运输船"
-  },
-  {
-    "civ": "mon",
-    "section": "船只",
-    "name": "轻型戎克船",
-    "img": "戎克船",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/轻型戎克船"
-  },
-  {
-    "civ": "mon",
-    "section": "船只",
-    "name": "戎克战船",
-    "img": "戎克战船",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/戎克战船"
-  },
-  {
-    "civ": "mon",
-    "section": "船只",
-    "name": "爆破型戎克船",
-    "img": "爆破型戎克船",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/爆破型戎克船"
-  },
-  {
-    "civ": "mon",
-    "section": "船只",
-    "name": "宝船",
-    "img": "宝船",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/宝船"
-  },
-  {
-    "civ": "mon",
-    "section": "黑暗时代建筑",
-    "name": "蒙古包",
-    "img": "蒙古包",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/蒙古包"
-  },
-  {
-    "civ": "mon",
-    "section": "黑暗时代建筑",
-    "name": "敖包",
-    "img": "敖包",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/敖包"
-  },
-  {
-    "civ": "mon",
-    "section": "黑暗时代建筑",
-    "name": "牧场",
-    "img": "牧场",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/牧场"
-  },
-  {
-    "civ": "mon",
-    "section": "黑暗时代建筑",
-    "name": "兵营",
-    "img": "兵营",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/兵营"
-  },
-  {
-    "civ": "mon",
-    "section": "黑暗时代建筑",
-    "name": "码头",
-    "img": "码头",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/码头"
-  },
-  {
-    "civ": "mon",
-    "section": "黑暗时代建筑",
-    "name": "哨站",
-    "img": "哨站",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/哨站"
-  },
-  {
-    "civ": "mon",
-    "section": "封建时代建筑",
-    "name": "铁匠铺",
-    "img": "铁匠铺",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/铁匠铺"
-  },
-  {
-    "civ": "mon",
-    "section": "封建时代建筑",
-    "name": "市场",
-    "img": "市场",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/市场"
-  },
-  {
-    "civ": "mon",
-    "section": "封建时代建筑",
-    "name": "城镇中心",
-    "img": "城镇中心",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/城镇中心"
-  },
-  {
-    "civ": "mon",
-    "section": "封建时代建筑",
-    "name": "靶场",
-    "img": "靶场",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/靶场"
-  },
-  {
-    "civ": "mon",
-    "section": "封建时代建筑",
-    "name": "马厩",
-    "img": "马厩",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/马厩"
-  },
-  {
-    "civ": "mon",
-    "section": "城堡时代建筑",
-    "name": "祈祷帐篷",
-    "img": "祈祷帐篷",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/祈祷帐篷"
-  },
-  {
-    "civ": "mon",
-    "section": "城堡时代建筑",
-    "name": "攻城武器厂",
-    "img": "攻城武器厂",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/攻城武器厂"
-  },
-  {
-    "civ": "mon",
-    "section": "帝王时代建筑",
-    "name": "大汗纪念碑",
-    "img": "大汗纪念碑",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/大汗纪念碑"
-  },
-  {
-    "civ": "mon",
-    "section": "地标建筑",
-    "name": "银之树",
-    "img": "银之树",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/银之树"
-  },
-  {
-    "civ": "mon",
-    "section": "地标建筑",
-    "name": "鹿石",
-    "img": "鹿石",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/鹿石"
-  },
-  {
-    "civ": "mon",
-    "section": "地标建筑",
-    "name": "草原防卫据点",
-    "img": "草原防卫据点",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/草原防卫据点"
-  },
-  {
-    "civ": "mon",
-    "section": "地标建筑",
-    "name": "忽里勒台",
-    "img": "忽里勒台",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/忽里勒台"
-  },
-  {
-    "civ": "mon",
-    "section": "地标建筑",
-    "name": "汗国宫殿",
-    "img": "汗国宫殿",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/汗国宫殿"
-  },
-  {
-    "civ": "mon",
-    "section": "地标建筑",
-    "name": "白色浮屠佛塔",
-    "img": "白色浮屠佛塔",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/白色浮屠佛塔"
-  },
+    {
+        "civ": "mon",
+        "section": "平民与宗教单位",
+        "name": "村民",
+        "img": "蒙古村民",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/村民"
+    },
+    {
+        "civ": "mon",
+        "section": "平民与宗教单位",
+        "name": "萨满",
+        "img": "萨满",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/萨满"
+    },
+    {
+        "civ": "mon",
+        "section": "平民与宗教单位",
+        "name": "商人",
+        "img": "商人",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/商人"
+    },
+    {
+        "civ": "mon",
+        "section": "平民与宗教单位",
+        "name": "君主",
+        "img": "君主",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/君主"
+    },
+    {
+        "civ": "mon",
+        "section": "军事单位",
+        "name": "长矛兵",
+        "img": "长矛兵",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/长矛兵"
+    },
+    {
+        "civ": "mon",
+        "section": "军事单位",
+        "name": "武士",
+        "img": "武士",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/武士"
+    },
+    {
+        "civ": "mon",
+        "section": "军事单位",
+        "name": "侦察兵",
+        "img": "侦察兵",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/侦察兵"
+    },
+    {
+        "civ": "mon",
+        "section": "军事单位",
+        "name": "骑手",
+        "img": "骑手",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/骑手"
+    },
+    {
+        "civ": "mon",
+        "section": "军事单位",
+        "name": "怯薛骑兵",
+        "img": "怯薛骑兵",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/怯薛骑兵"
+    },
+    {
+        "civ": "mon",
+        "section": "军事单位",
+        "name": "步弓手",
+        "img": "步弓手",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/步弓手"
+    },
+    {
+        "civ": "mon",
+        "section": "军事单位",
+        "name": "弩手",
+        "img": "弩手",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/弩手"
+    },
+    {
+        "civ": "mon",
+        "section": "军事单位",
+        "name": "火枪兵",
+        "img": "火枪兵",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/火枪兵"
+    },
+    {
+        "civ": "mon",
+        "section": "军事单位",
+        "name": "蒙古突骑",
+        "img": "蒙古突骑",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/蒙古突骑"
+    },
+    {
+        "civ": "mon",
+        "section": "军事单位",
+        "name": "汗的猎人",
+        "img": "汗的猎人",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/汗的猎人"
+    },
+    {
+        "civ": "mon",
+        "section": "军事单位",
+        "name": "汗",
+        "img": "汗",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/汗"
+    },
+    {
+        "civ": "mon",
+        "section": "攻城武器",
+        "name": "扭力弩炮",
+        "img": "扭力弩炮",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/扭力弩炮"
+    },
+    {
+        "civ": "mon",
+        "section": "攻城武器",
+        "name": "轻型投石车",
+        "img": "轻型投石车",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/轻型投石车"
+    },
+    {
+        "civ": "mon",
+        "section": "攻城武器",
+        "name": "牵引式巨型投石机",
+        "img": "牵引式巨型投石机",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/牵引式巨型投石机"
+    },
+    {
+        "civ": "mon",
+        "section": "攻城武器",
+        "name": "手推炮",
+        "img": "手推炮",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/手推炮"
+    },
+    {
+        "civ": "mon",
+        "section": "攻城武器",
+        "name": "攻城塔",
+        "img": "攻城塔",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/攻城塔"
+    },
+    {
+        "civ": "mon",
+        "section": "攻城武器",
+        "name": "攻城锤",
+        "img": "攻城锤",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/攻城锤"
+    },
+    {
+        "civ": "mon",
+        "section": "攻城武器",
+        "name": "回回炮",
+        "img": "回回炮",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/回回炮"
+    },
+    {
+        "civ": "mon",
+        "section": "船只",
+        "name": "渔船",
+        "img": "渔船",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/渔船"
+    },
+    {
+        "civ": "mon",
+        "section": "船只",
+        "name": "商船",
+        "img": "商船",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/商船"
+    },
+    {
+        "civ": "mon",
+        "section": "船只",
+        "name": "运输船",
+        "img": "运输船",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/运输船"
+    },
+    {
+        "civ": "mon",
+        "section": "船只",
+        "name": "轻型戎克船",
+        "img": "戎克船",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/轻型戎克船"
+    },
+    {
+        "civ": "mon",
+        "section": "船只",
+        "name": "戎克战船",
+        "img": "戎克战船",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/戎克战船"
+    },
+    {
+        "civ": "mon",
+        "section": "船只",
+        "name": "爆破型戎克船",
+        "img": "爆破型戎克船",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/爆破型戎克船"
+    },
+    {
+        "civ": "mon",
+        "section": "船只",
+        "name": "宝船",
+        "img": "宝船",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/宝船"
+    },
+    {
+        "civ": "mon",
+        "section": "黑暗时代建筑",
+        "name": "蒙古包",
+        "img": "蒙古包",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/蒙古包"
+    },
+    {
+        "civ": "mon",
+        "section": "黑暗时代建筑",
+        "name": "敖包",
+        "img": "敖包",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/敖包"
+    },
+    {
+        "civ": "mon",
+        "section": "黑暗时代建筑",
+        "name": "牧场",
+        "img": "牧场",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/牧场"
+    },
+    {
+        "civ": "mon",
+        "section": "黑暗时代建筑",
+        "name": "兵营",
+        "img": "兵营",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/兵营"
+    },
+    {
+        "civ": "mon",
+        "section": "黑暗时代建筑",
+        "name": "码头",
+        "img": "码头",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/码头"
+    },
+    {
+        "civ": "mon",
+        "section": "黑暗时代建筑",
+        "name": "哨站",
+        "img": "哨站",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/哨站"
+    },
+    {
+        "civ": "mon",
+        "section": "黑暗时代建筑",
+        "name": "马厩",
+        "img": "马厩",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/马厩"
+    },
+    {
+        "civ": "mon",
+        "section": "封建时代建筑",
+        "name": "铁匠铺",
+        "img": "铁匠铺",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/铁匠铺"
+    },
+    {
+        "civ": "mon",
+        "section": "封建时代建筑",
+        "name": "市场",
+        "img": "市场",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/市场"
+    },
+    {
+        "civ": "mon",
+        "section": "封建时代建筑",
+        "name": "城镇中心",
+        "img": "城镇中心",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/城镇中心"
+    },
+    {
+        "civ": "mon",
+        "section": "封建时代建筑",
+        "name": "靶场",
+        "img": "靶场",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/靶场"
+    },
+    {
+        "civ": "mon",
+        "section": "城堡时代建筑",
+        "name": "祈祷帐篷",
+        "img": "祈祷帐篷",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/祈祷帐篷"
+    },
+    {
+        "civ": "mon",
+        "section": "城堡时代建筑",
+        "name": "攻城武器厂",
+        "img": "攻城武器厂",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/攻城武器厂"
+    },
+    {
+        "civ": "mon",
+        "section": "帝王时代建筑",
+        "name": "大汗纪念碑",
+        "img": "大汗纪念碑",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/大汗纪念碑"
+    },
+    {
+        "civ": "mon",
+        "section": "地标建筑",
+        "name": "银之树",
+        "img": "银之树",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/银之树"
+    },
+    {
+        "civ": "mon",
+        "section": "地标建筑",
+        "name": "鹿石",
+        "img": "鹿石",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/鹿石"
+    },
+    {
+        "civ": "mon",
+        "section": "地标建筑",
+        "name": "草原防卫据点",
+        "img": "草原防卫据点",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/草原防卫据点"
+    },
+    {
+        "civ": "mon",
+        "section": "地标建筑",
+        "name": "忽里勒台",
+        "img": "忽里勒台",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/忽里勒台"
+    },
+    {
+        "civ": "mon",
+        "section": "地标建筑",
+        "name": "汗国宫殿",
+        "img": "汗国宫殿",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/汗国宫殿"
+    },
+    {
+        "civ": "mon",
+        "section": "地标建筑",
+        "name": "白色浮屠佛塔",
+        "img": "白色浮屠佛塔",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/白色浮屠佛塔"
+    },
 ];
