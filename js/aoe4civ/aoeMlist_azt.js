e4eca0e8c61472c3c9a225652ba771a32f246edd
seicing <1289846385@qq.com>
2026-04-16 13:41:30 +0800

    2026-04-16-13:40:13

---
+++ js/aoe4civ/aoeMlist_azt.js
@@ -0,0 +1,198 @@
+export const aoe4list = [
+    {
+        "civ": "azt",
+        "section": "平民单位",
+        "name": "村民",
+        "img": "村民",
+        "link": "https://seicing.com/html/aoe2/unitaoem/村民"
+    },
+    {
+        "civ": "azt",
+        "section": "平民单位",
+        "name": "毛驴篷车",
+        "img": "毛驴篷车",
+        "link": "https://seicing.com/html/aoe2/unitaoem/毛驴篷车"
+    },
+    {
+        "civ": "azt",
+        "section": "军事单位",
+        "name": "重甲兵",
+        "img": "重甲兵",
+        "link": "https://seicing.com/html/aoe2/unitaoem/重甲兵"
+    },
+    {
+        "civ": "azt",
+        "section": "船只",
+        "name": "捕鱼船",
+        "img": "阿兹特克捕鱼船",
+        "link": "https://seicing.com/html/aoe2/unitaoem/捕鱼船"
+    },
+    {
+        "civ": "azt",
+        "section": "船只",
+        "name": "运输船",
+        "img": "阿兹特克运输船",
+        "link": "https://seicing.com/html/aoe2/unitaoem/运输船"
+    },
+    {
+        "civ": "azt",
+        "section": "船只",
+        "name": "三列桨座战船",
+        "img": "三列桨座战船",
+        "link": "https://seicing.com/html/aoe2/unitaoem/三列桨座战船"
+    },
+    {
+        "civ": "azt",
+        "section": "船只",
+        "name": "希腊冲击船",
+        "img": "希腊冲击船",
+        "link": "https://seicing.com/html/aoe2/unitaoem/希腊冲击船"
+    },
+    {
+        "civ": "azt",
+        "section": "船只",
+        "name": "攻城巨舰",
+        "img": "攻城巨舰",
+        "link": "https://seicing.com/html/aoe2/unitaoem/攻城巨舰"
+    },
+    {
+        "civ": "azt",
+        "section": "英雄单位",
+        "name": "伊阿宋",
+        "img": "伊阿宋",
+        "link": "https://seicing.com/html/aoe2/unitaoem/伊阿宋"
+    },
+    {
+        "civ": "azt",
+        "section": "神话单位",
+        "name": "珀伽索斯",
+        "img": "珀伽索斯",
+        "link": "https://seicing.com/html/aoe2/unitaoem/珀伽索斯"
+    },
+    {
+        "civ": "azt",
+        "section": "神话单位",
+        "name": "泰坦",
+        "img": "阿兹特克泰坦",
+        "link": "https://seicing.com/html/aoe2/unitaoem/泰坦"
+    },
+    {
+        "civ": "azt",
+        "section": "常规建筑",
+        "name": "城镇中心",
+        "img": "城镇中心阿兹特克",
+        "link": "https://seicing.com/html/aoe2/buildingaoem/城镇中心"
+    },
+    {
+        "civ": "azt",
+        "section": "常规建筑",
+        "name": "村庄中心",
+        "img": "村庄中心阿兹特克",
+        "link": "https://seicing.com/html/aoe2/buildingaoem/村庄中心"
+    },
+    {
+        "civ": "azt",
+        "section": "常规建筑",
+        "name": "房屋",
+        "img": "房屋",
+        "link": "https://seicing.com/html/aoe2/buildingaoem/房屋"
+    },
+    {
+        "civ": "azt",
+        "section": "常规建筑",
+        "name": "谷仓",
+        "img": "谷仓？",
+        "link": "https://seicing.com/html/aoe2/buildingaoem/谷仓"
+    },
+    {
+        "civ": "azt",
+        "section": "常规建筑",
+        "name": "农田",
+        "img": "农田",
+        "link": "https://seicing.com/html/aoe2/buildingaoem/农田"
+    },
+    {
+        "civ": "azt",
+        "section": "常规建筑",
+        "name": "军械库",
+        "img": "军械库",
+        "link": "https://seicing.com/html/aoe2/buildingaoem/军械库"
+    },
+    {
+        "civ": "azt",
+        "section": "常规建筑",
+        "name": "市场",
+        "img": "市场",
+        "link": "https://seicing.com/html/aoe2/buildingaoem/市场"
+    },
+    {
+        "civ": "azt",
+        "section": "常规建筑",
+        "name": "仓库",
+        "img": "仓库？",
+        "link": "https://seicing.com/html/aoe2/buildingaoem/仓库"
+    },
+    {
+        "civ": "azt",
+        "section": "常规建筑",
+        "name": "世界奇观",
+        "img": "世界奇观",
+        "link": "https://seicing.com/html/aoe2/buildingaoem/世界奇观"
+    },
+    {
+        "civ": "azt",
+        "section": "军事建筑",
+        "name": "码头",
+        "img": "码头",
+        "link": "https://seicing.com/html/aoe2/buildingaoem/码头"
+    },
+    {
+        "civ": "azt",
+        "section": "军事建筑",
+        "name": "军事学院",
+        "img": "军事学院？",
+        "link": "https://seicing.com/html/aoe2/buildingaoem/军事学院"
+    },
+    {
+        "civ": "azt",
+        "section": "军事建筑",
+        "name": "马厩",
+        "img": "马厩？",
+        "link": "https://seicing.com/html/aoe2/buildingaoem/马厩"
+    },
+    {
+        "civ": "azt",
+        "section": "军事建筑",
+        "name": "靶场",
+        "img": "靶场？",
+        "link": "https://seicing.com/html/aoe2/buildingaoem/靶场"
+    },
+    {
+        "civ": "azt",
+        "section": "军事建筑",
+        "name": "圣殿",
+        "img": "圣殿",
+        "link": "https://seicing.com/html/aoe2/buildingaoem/圣殿"
+    },
+    {
+        "civ": "azt",
+        "section": "军事建筑",
+        "name": "要塞",
+        "img": "要塞？",
+        "link": "https://seicing.com/html/aoe2/buildingaoem/要塞"
+    },
+    {
+        "civ": "azt",
+        "section": "军事建筑",
+        "name": "木制城墙",
+        "img": "木制城墙",
+        "link": "https://seicing.com/html/aoe2/buildingaoem/木制城墙"
+    },
+    {
+        "civ": "azt",
+        "section": "军事建筑",
+        "name": "警卫塔",
+        "img": "警卫塔",
+        "link": "https://seicing.com/html/aoe2/buildingaoem/警卫塔"
+    }
+];
