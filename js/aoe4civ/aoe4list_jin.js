2dd16af98b52e12c5807cdb819a84b024e02c92b
seicing <1289846385@qq.com>
2026-05-04 23:02:41 +0800

    2026-05-04-23:01:50

---
+++ js/aoe4civ/aoe4list_jin.js
@@ -0,0 +1,415 @@
+export const aoe4list = [
+  {
+    "civ": "jin",
+    "section": "平民与宗教单位",
+    "name": "村民",
+    "img": "中国村民",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/村民"
+  },
+  {
+    "civ": "jin",
+    "section": "平民与宗教单位",
+    "name": "乡骑",
+    "img": "乡骑",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/乡骑"
+  },
+  {
+    "civ": "jin",
+    "section": "平民与宗教单位",
+    "name": "使者",
+    "img": "使者",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/使者"
+  },
+  {
+    "civ": "jin",
+    "section": "平民与宗教单位",
+    "name": "僧侣",
+    "img": "僧侣",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/僧侣"
+  },
+  {
+    "civ": "jin",
+    "section": "平民与宗教单位",
+    "name": "驯鹿商人",
+    "img": "驯鹿商人",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/驯鹿商人"
+  },
+  {
+    "civ": "jin",
+    "section": "平民与宗教单位",
+    "name": "君主",
+    "img": "君主",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/君主"
+  },
+  {
+    "civ": "jin",
+    "section": "军事单位",
+    "name": "长矛兵",
+    "img": "长矛兵",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/长矛兵"
+  },
+  {
+    "civ": "jin",
+    "section": "军事单位",
+    "name": "武士",
+    "img": "武士",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/武士"
+  },
+  {
+    "civ": "jin",
+    "section": "军事单位",
+    "name": "斩马刀",
+    "img": "斩马刀",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/斩马刀"
+  },
+  {
+    "civ": "jin",
+    "section": "军事单位",
+    "name": "侦察兵",
+    "img": "侦察兵",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/侦察兵"
+  },
+  {
+    "civ": "jin",
+    "section": "军事单位",
+    "name": "骑手",
+    "img": "骑手",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/骑手"
+  },
+  {
+    "civ": "jin",
+    "section": "军事单位",
+    "name": "铁浮屠",
+    "img": "铁浮屠",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/铁浮屠"
+  },
+  {
+    "civ": "jin",
+    "section": "军事单位",
+    "name": "猛安谋克守军",
+    "img": "猛安谋克守军",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/猛安谋克守军"
+  },
+  {
+    "civ": "jin",
+    "section": "军事单位",
+    "name": "靺鞨族人",
+    "img": "靺鞨族人",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/靺鞨族人"
+  },
+  {
+    "civ": "jin",
+    "section": "军事单位",
+    "name": "弩手",
+    "img": "弩手",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/弩手"
+  },
+  {
+    "civ": "jin",
+    "section": "军事单位",
+    "name": "爆破手",
+    "img": "爆破手",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/爆破手"
+  },
+  {
+    "civ": "jin",
+    "section": "军事单位",
+    "name": "骑马掷弹兵",
+    "img": "骑马掷弹兵",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/骑马掷弹兵"
+  },
+  {
+    "civ": "jin",
+    "section": "攻城武器",
+    "name": "弓床弩",
+    "img": "弓床弩",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/弓床弩"
+  },
+  {
+    "civ": "jin",
+    "section": "攻城武器",
+    "name": "一窝蜂",
+    "img": "一窝蜂",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/一窝蜂"
+  },
+  {
+    "civ": "jin",
+    "section": "攻城武器",
+    "name": "牵引式巨型投石机",
+    "img": "牵引式巨型投石机",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/牵引式巨型投石机"
+  },
+  {
+    "civ": "jin",
+    "section": "攻城武器",
+    "name": "手推炮",
+    "img": "手推炮",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/手推炮"
+  },
+  {
+    "civ": "jin",
+    "section": "攻城武器",
+    "name": "攻城塔",
+    "img": "攻城塔",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/攻城塔"
+  },
+  {
+    "civ": "jin",
+    "section": "攻城武器",
+    "name": "攻城锤",
+    "img": "攻城锤",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/攻城锤"
+  },
+  {
+    "civ": "jin",
+    "section": "船只",
+    "name": "渔船",
+    "img": "渔船",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/渔船"
+  },
+  {
+    "civ": "jin",
+    "section": "船只",
+    "name": "商船",
+    "img": "商船",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/商船"
+  },
+  {
+    "civ": "jin",
+    "section": "船只",
+    "name": "运输船",
+    "img": "运输船",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/运输船"
+  },
+  {
+    "civ": "jin",
+    "section": "船只",
+    "name": "戎克船",
+    "img": "戎克船",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/步弓手船"
+  },
+  {
+    "civ": "jin",
+    "section": "船只",
+    "name": "戎克战船",
+    "img": "戎克战船",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/扭力弩炮船"
+  },
+  {
+    "civ": "jin",
+    "section": "船只",
+    "name": "爆破型戎克船",
+    "img": "爆破型戎克船",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/爆破船"
+  },
+  {
+    "civ": "jin",
+    "section": "船只",
+    "name": "宝船",
+    "img": "宝船",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/战船"
+  },
+  {
+    "civ": "jin",
+    "section": "黑暗时代建筑",
+    "name": "房屋",
+    "img": "房屋",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/房屋"
+  },
+  {
+    "civ": "jin",
+    "section": "黑暗时代建筑",
+    "name": "磨坊",
+    "img": "磨坊",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/磨坊"
+  },
+  {
+    "civ": "jin",
+    "section": "黑暗时代建筑",
+    "name": "伐木场",
+    "img": "伐木场",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/伐木场"
+  },
+  {
+    "civ": "jin",
+    "section": "黑暗时代建筑",
+    "name": "采矿场",
+    "img": "采矿场",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/采矿场"
+  },
+  {
+    "civ": "jin",
+    "section": "黑暗时代建筑",
+    "name": "农田",
+    "img": "农田",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/农田"
+  },
+  {
+    "civ": "jin",
+    "section": "黑暗时代建筑",
+    "name": "兵营",
+    "img": "兵营",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/兵营"
+  },
+  {
+    "civ": "jin",
+    "section": "黑暗时代建筑",
+    "name": "码头",
+    "img": "码头",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/码头"
+  },
+  {
+    "civ": "jin",
+    "section": "黑暗时代建筑",
+    "name": "哨站",
+    "img": "哨站",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/哨站"
+  },
+  {
+    "civ": "jin",
+    "section": "黑暗时代建筑",
+    "name": "木栅栏",
+    "img": "木栅栏",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/木栅栏"
+  },
+  {
+    "civ": "jin",
+    "section": "黑暗时代建筑",
+    "name": "木城门",
+    "img": "木城门",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/木城门"
+  },
+  {
+    "civ": "jin",
+    "section": "封建时代建筑",
+    "name": "铁匠铺",
+    "img": "铁匠铺",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/铁匠铺"
+  },
+  {
+    "civ": "jin",
+    "section": "封建时代建筑",
+    "name": "市场",
+    "img": "市场",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/市场"
+  },
+  {
+    "civ": "jin",
+    "section": "封建时代建筑",
+    "name": "城镇中心",
+    "img": "城镇中心",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/城镇中心"
+  },
+  {
+    "civ": "jin",
+    "section": "封建时代建筑",
+    "name": "机械车间",
+    "img": "机械车间",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/机械车间"
+  },
+  {
+    "civ": "jin",
+    "section": "封建时代建筑",
+    "name": "战争马厩",
+    "img": "战争马厩",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/战争马厩"
+  },
+  {
+    "civ": "jin",
+    "section": "封建时代建筑",
+    "name": "草原",
+    "img": "草原",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/草原"
+  },
+  {
+    "civ": "jin",
+    "section": "封建时代建筑",
+    "name": "石墙塔",
+    "img": "石墙塔",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/石墙塔"
+  },
+  {
+    "civ": "jin",
+    "section": "封建时代建筑",
+    "name": "石墙",
+    "img": "石墙",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/石墙"
+  },
+  {
+    "civ": "jin",
+    "section": "封建时代建筑",
+    "name": "石墙城门",
+    "img": "石墙城门",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/石墙城门"
+  },
+  {
+    "civ": "jin",
+    "section": "城堡时代建筑",
+    "name": "修道院",
+    "img": "修道院",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/修道院"
+  },
+  {
+    "civ": "jin",
+    "section": "城堡时代建筑",
+    "name": "城堡",
+    "img": "城堡",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/城堡"
+  },
+  {
+    "civ": "jin",
+    "section": "帝王时代建筑",
+    "name": "大学",
+    "img": "大学",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/大学"
+  },
+  {
+    "civ": "jin",
+    "section": "帝王时代建筑",
+    "name": "帝王飞地",
+    "img": "帝王飞地",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/世界奇观"
+  },
+  {
+    "civ": "jin",
+    "section": "地标建筑",
+    "name": "花塔",
+    "img": "花塔",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/花塔"
+  },
+  {
+    "civ": "jin",
+    "section": "地标建筑",
+    "name": "大牧场",
+    "img": "大牧场",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/大牧场"
+  },
+  {
+    "civ": "jin",
+    "section": "地标建筑",
+    "name": "龙亭",
+    "img": "龙亭",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/龙亭"
+  },
+  {
+    "civ": "jin",
+    "section": "地标建筑",
+    "name": "山门殿",
+    "img": "山门殿",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/山门殿"
+  },
+  {
+    "civ": "jin",
+    "section": "地标建筑",
+    "name": "长城堡垒",
+    "img": "长城堡垒",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/长城堡垒"
+  },
+  {
+    "civ": "jin",
+    "section": "地标建筑",
+    "name": "银山塔林",
+    "img": "银山塔林",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/银山塔林"
+  },
+];
