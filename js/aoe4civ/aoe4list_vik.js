27f4749a71dd148bfc89b3a2c541acf935362dd0
seicing <1289846385@qq.com>
2026-06-08 20:04:33 +0800

    upload

---
+++ js/aoe4civ/aoe4list_vik.js
@@ -0,0 +1,207 @@
+export const aoe4list = [
+  {
+    "civ": "vik",
+    "section": "平民与宗教单位",
+    "name": "村民",
+    "img": "村民",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/村民"
+  },
+
+
+  {
+    "civ": "vik",
+    "section": "平民与宗教单位",
+    "name": "君主",
+    "img": "君主",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/君主"
+  },
+
+
+  {
+    "civ": "vik",
+    "section": "军事单位",
+    "name": "狂战士",
+    "img": "狂战士",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/狂战士"
+  },
+  {
+    "civ": "vik",
+    "section": "军事单位",
+    "name": "近卫军",
+    "img": "近卫军",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/近卫军"
+  },
+  {
+    "civ": "vik",
+    "section": "军事单位",
+    "name": "盾女",
+    "img": "盾女",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/盾女"
+  },
+
+
+
+  {
+    "civ": "vik",
+    "section": "军事单位",
+    "name": "骑士",
+    "img": "骑士",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/骑士"
+  },
+
+  {
+    "civ": "vik",
+    "section": "军事单位",
+    "name": "民兵弩手",
+    "img": "民兵弩手",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/民兵弩手"
+  },
+
+
+  {
+    "civ": "vik",
+    "section": "攻城武器",
+    "name": "扭力弩炮",
+    "img": "扭力弩炮",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/扭力弩炮"
+  },
+  {
+    "civ": "vik",
+    "section": "攻城武器",
+    "name": "轻型投石车",
+    "img": "轻型投石车",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/轻型投石车"
+  },
+  {
+    "civ": "vik",
+    "section": "攻城武器",
+    "name": "配重式巨型投石机",
+    "img": "配重式巨型投石机",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/配重式巨型投石机"
+  },
+  {
+    "civ": "vik",
+    "section": "攻城武器",
+    "name": "手推炮",
+    "img": "手推炮",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/手推炮"
+  },
+  {
+    "civ": "vik",
+    "section": "攻城武器",
+    "name": "攻城塔",
+    "img": "攻城塔",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/攻城塔"
+  },
+  {
+    "civ": "vik",
+    "section": "攻城武器",
+    "name": "攻城锤",
+    "img": "攻城锤",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/攻城锤"
+  },
+
+
+
+  {
+    "civ": "vik",
+    "section": "船只",
+    "name": "渔船",
+    "img": "渔船",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/渔船"
+  },
+  {
+    "civ": "vik",
+    "section": "船只",
+    "name": "商船",
+    "img": "商船",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/商船"
+  },
+  {
+    "civ": "vik",
+    "section": "船只",
+    "name": "运输船",
+    "img": "运输船",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/运输船"
+  },
+  {
+    "civ": "vik",
+    "section": "船只",
+    "name": "维京长船",
+    "img": "维京长船",
+    "link": "https://seicing.com/html/aoe2/unitaoe4/维京长船"
+  },
+
+
+
+  {
+    "civ": "vik",
+    "section": "黑暗时代建筑",
+    "name": "长屋",
+    "img": "长屋",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/长屋"
+  },
+  {
+    "civ": "vik",
+    "section": "黑暗时代建筑",
+    "name": "磨坊",
+    "img": "磨坊",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/磨坊"
+  },
+  {
+    "civ": "vik",
+    "section": "黑暗时代建筑",
+    "name": "伐木场",
+    "img": "伐木场",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/伐木场"
+  },
+  {
+    "civ": "vik",
+    "section": "黑暗时代建筑",
+    "name": "采矿场",
+    "img": "采矿场",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/采矿场"
+  },
+  {
+    "civ": "vik",
+    "section": "黑暗时代建筑",
+    "name": "农田",
+    "img": "农田",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/农田"
+  },
+
+
+
+
+  {
+    "civ": "vik",
+    "section": "黑暗时代建筑",
+    "name": "木栅栏",
+    "img": "木栅栏",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/木栅栏"
+  },
+  {
+    "civ": "vik",
+    "section": "黑暗时代建筑",
+    "name": "木城门",
+    "img": "木城门",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/木城门"
+  },
+
+
+  {
+    "civ": "vik",
+    "section": "地标建筑",
+    "name": "‌木板教堂",
+    "img": "‌木板教堂",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/‌木板教堂"
+  },
+  {
+    "civ": "vik",
+    "section": "地标建筑",
+    "name": "环形要塞",
+    "img": "环形要塞",
+    "link": "https://seicing.com/html/aoe2/buildingsaoe4/landmark/环形要塞"
+  },
+
+];
