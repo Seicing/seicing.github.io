0574a9a5cfeb8717c5874b43457c88994f1fada5
seicing <1289846385@qq.com>
2026-07-13 23:55:53 +0800

    upload

--- js/lavipage2.js
+++ js/lavipage2.js
@@ -1,5 +1,6 @@
 var lavipage = document.getElementById("lavipage");
 var page = lavipage.getAttribute("page");
 var label = document.getElementById("page_" + page);
-label.style.color = "#6b1e1e";
-label.style.fontWeight = "bold";
+if (label) {
+    label.classList.add("active-page"); // 通过动态添加 class 让 CSS 自动控制色彩
+}
