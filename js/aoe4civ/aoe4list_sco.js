23d0ad09125efc6d03e619cf8d218030be30be43
seicing <1289846385@qq.com>
2026-07-24 13:29:09 +0800

    upload

--- js/aoe4civ/aoe4list_sco.js
+++ js/aoe4civ/aoe4list_sco.js
@@ -1,60 +1,60 @@
 export const aoe4list = [
-  {
-    "civ": "sco",
-    "section": "平民与宗教单位",
-    "name": "村民",
-    "img": "村民",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/村民"
-  },
+    {
+        "civ": "sco",
+        "section": "平民与宗教单位",
+        "name": "村民",
+        "img": "村民",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/村民"
+    },
 
 
-  {
-    "civ": "sco",
-    "section": "平民与宗教单位",
-    "name": "君主",
-    "img": "君主",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/君主"
-  },
+    {
+        "civ": "sco",
+        "section": "平民与宗教单位",
+        "name": "君主",
+        "img": "君主",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/君主"
+    },
 
 
-  {
-    "civ": "sco",
-    "section": "军事单位",
-    "name": "笼手剑士",
-    "img": "笼手剑士",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/笼手剑士"
-  },
-  {
-    "civ": "sco",
-    "section": "军事单位",
-    "name": "苏格兰佣兵",
-    "img": "苏格兰佣兵",
-    "link": "https://seicing.com/html/aoe2/unitaoe4/苏格兰佣兵"
-  },
+    {
+        "civ": "sco",
+        "section": "军事单位",
+        "name": "氏族战士",
+        "img": "氏族战士",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/氏族战士"
+    },
+    {
+        "civ": "sco",
+        "section": "军事单位",
+        "name": "加洛格拉什",
+        "img": "加洛格拉什",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/加洛格拉什"
+    },
 
 
 
-  {
-    "civ": "sco",
-    "section": "黑暗时代建筑",
-    "name": "塔楼房屋",
-    "img": "塔楼房屋",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/塔楼房屋"
-  },
-  {
-    "civ": "sco",
-    "section": "黑暗时代建筑",
-    "name": "集结大厅",
-    "img": "集结大厅",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/集结大厅"
-  },
+    {
+        "civ": "sco",
+        "section": "黑暗时代建筑",
+        "name": "塔楼房屋",
+        "img": "塔楼房屋",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/塔楼房屋"
+    },
+    {
+        "civ": "sco",
+        "section": "黑暗时代建筑",
+        "name": "集结大厅",
+        "img": "集结大厅",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/集结大厅"
+    },
 
 
-  {
-    "civ": "sco",
-    "section": "城堡时代建筑",
-    "name": "城堡",
-    "img": "城堡",
-    "link": "https://seicing.com/html/aoe2/buildingsaoe4/城堡"
-  },
+    {
+        "civ": "sco",
+        "section": "城堡时代建筑",
+        "name": "城堡",
+        "img": "城堡",
+        "link": "https://seicing.com/html/aoe2/buildingsaoe4/城堡"
+    },
 ];
