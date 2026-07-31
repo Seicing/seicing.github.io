9253096a5090b4c978377a548b09a030f6e45280
seicing <1289846385@qq.com>
2026-07-31 09:28:55 +0800

    upload

--- js/aoe4civ/aoe4list_sco.js
+++ js/aoe4civ/aoe4list_sco.js
@@ -6,8 +6,13 @@ export const aoe4list = [
         "img": "村民",
         "link": "https://seicing.com/html/aoe2/unitaoe4/村民"
     },
-
-
+    {
+        "civ": "sco",
+        "section": "平民与宗教单位",
+        "name": "僧侣",
+        "img": "僧侣",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/僧侣"
+    },
     {
         "civ": "sco",
         "section": "平民与宗教单位",
@@ -17,6 +22,20 @@ export const aoe4list = [
     },
 
 
+    {
+        "civ": "sco",
+        "section": "军事单位",
+        "name": "长矛兵",
+        "img": "长矛兵",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/长矛兵"
+    },
+    {
+        "civ": "sco",
+        "section": "军事单位",
+        "name": "武士",
+        "img": "武士",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/武士"
+    },
     {
         "civ": "sco",
         "section": "军事单位",
@@ -30,6 +49,50 @@ export const aoe4list = [
         "name": "加洛格拉什",
         "img": "加洛格拉什",
         "link": "https://seicing.com/html/aoe2/unitaoe4/加洛格拉什"
+    },
+
+    {
+        "civ": "sco",
+        "section": "军事单位",
+        "name": "侦察兵",
+        "img": "侦察兵",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/侦察兵"
+    },
+    {
+        "civ": "sco",
+        "section": "军事单位",
+        "name": "骑手",
+        "img": "骑手",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/骑手"
+    },
+    {
+        "civ": "sco",
+        "section": "军事单位",
+        "name": "边境突袭者",
+        "img": "边境突袭者",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/边境突袭者"
+    },
+
+    {
+        "civ": "sco",
+        "section": "军事单位",
+        "name": "步弓手",
+        "img": "步弓手",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/步弓手"
+    },
+    {
+        "civ": "sco",
+        "section": "军事单位",
+        "name": "弩手",
+        "img": "弩手",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/弩手"
+    },
+    {
+        "civ": "sco",
+        "section": "军事单位",
+        "name": "火枪兵",
+        "img": "火枪兵",
+        "link": "https://seicing.com/html/aoe2/unitaoe4/火枪兵"
     },
 
 
